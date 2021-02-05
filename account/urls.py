81b1e6c090431cc6ac2b99e2ba1a0097fa79a87e
Hashim <hashimc@23andme.com>
2021-02-04 22:54:38 -0800

    Add user follows and basic actions

--- account/urls.py
+++ account/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
     path('edit/', views.edit, name='edit'),
+    path('users/follow/', views.user_follow, name='user_follow'),
     path('users/', views.user_list, name='user_list'),
     path('users/<username>/', views.user_detail, name='user_detail'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm, UserRegistrationForm, \
@@ -8,6 +8,9 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
+from django.views.decorators.http import require_POST
+from common.decorators import ajax_required
+from .models import Contact
 
 
 @login_required
@@ -28,6 +31,32 @@ def user_detail(request, username):
         'user': user
     }
     return render(request, 'account/user/detail.html', ctx)
+
+
+@ajax_required
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(
+                    user_from=request.user,
+                    user_to=user
+                )
+            else:
+                Contact.objects.filter(
+                    user_from=request.user,
+                    user_to=user).delete()
+            return JsonResponse({'status': 'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'error'})
+    return JsonResponse({'status': 'error'})
+
+
 
 
 @login_required
